d7efa9ddf4fc44254342e475037f57648100b7d2
Blewkse <julesbloux@gmail.com>
2024-10-18 16:17:01 +0200

    add anotehr cron to test

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.get("/", (req, res) => {
   console.log("accueil");
   res.status(200).json({ success: true, msg: new Date() });
 });
-app.get("/cron", handleCron);
+app.get("/cron1", handleCron.cron1);
+app.get("/cron2", handleCron.cron2);
 
 const port = process.env.PORT || 3100;
 
--- cron.js
+++ cron.js
@@ -1,4 +1,13 @@
-module.exports = (req, res) => {
-  console.log("mouahaha");
-  res.status(200).json({ success: true, msg: "Cron job executed" });
+const cron = {
+  cron1(req, res) {
+    console.log("Je suis le Cron N1");
+    res.status(200).json({ success: true, msg: "Cron job executed" });
+  },
+
+  cron2(req, res) {
+    console.log("Je suis le Cron N2");
+    res.status(200).json({ success: true, msg: "Cron job executed" });
+  },
 };
+
+module.exports = cron;
